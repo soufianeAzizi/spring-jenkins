f2e985a2d3a5a0461c1c4e8fbecfa6bd0a620a0d
soufianeAzizi <mbafianso@gmail.com>
2024-02-16 15:09:09 +0100

    SC

--- src/main/java/com/javatechie/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/springjenkins/SpringJenkinsApplication.java
@@ -5,13 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
+
+
   public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
   @PostConstruct
-  public void intt(){
+  public void initt (){
     logger.info("Application started ...");
   }
 
