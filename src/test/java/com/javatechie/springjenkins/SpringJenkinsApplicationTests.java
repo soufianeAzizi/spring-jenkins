16bf01ffebecf6993221246daa8537b28b74fbc3
soufianeAzizi <mbafianso@gmail.com>
2024-02-16 15:38:51 +0100

    TC

--- src/test/java/com/javatechie/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,19 +1,23 @@
 package com.javatechie.springjenkins;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkinsApplicationTests {
   public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
   @Test
   void contextLoads() {
-    logger.info("Test case executing");
+    logger.info("Test case executing ...");
+    logger.info("Test case executing sec log statement ...");
     assertEquals(true,true);
   }
 
