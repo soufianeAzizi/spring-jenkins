d01b2e2e275f7438d9523eb3b78010e9257a3504
soufianeAzizi <mbafianso@gmail.com>
2024-02-17 10:42:16 +0100

    4th commit

--- src/test/java/com/javatechie/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/springjenkins/SpringJenkinsApplicationTests.java
@@ -17,7 +17,7 @@ class SpringJenkinsApplicationTests {
   @Test
   void contextLoads() {
     logger.info("Test case executing ...");
-    logger.info("Test case executing sec log statement ...");
+    logger.info("Test case executing second log statement ...");
     assertEquals(true,true);
   }
 
